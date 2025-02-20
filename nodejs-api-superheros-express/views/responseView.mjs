3ad295f21019a28a9bf9b5a394dae43a1b3bf363
Joreynoso <joreynoso.developer@gmail.com>
2025-02-20 20:54:23 -0300

    Eliminar node_modules

---
+++ nodejs-api-superheros-express/views/responseView.mjs
@@ -0,0 +1,8 @@
+// SuperHero Views
+export function renderizarSupeHero(superheroe) {
+    return JSON.stringify(superheroe, null, 2);
+};
+
+export function renderizarListaSupherHero(superheroes) {
+    return JSON.stringify(superheroes, null, 2)
+};
