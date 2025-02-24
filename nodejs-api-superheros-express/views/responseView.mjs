88bd8eb6d85ea56ca138cf629f2f0ab80480aaf3
Joreynoso <joreynoso.developer@gmail.com>
2025-02-24 16:04:18 -0300

    Borrando duplicado

--- nodejs-api-superheros-express/views/responseView.mjs
+++
@@ -1,8 +0,0 @@
-// SuperHero Views
-export function renderizarSupeHero(superheroe) {
-    return JSON.stringify(superheroe, null, 2);
-};
-
-export function renderizarListaSupherHero(superheroes) {
-    return JSON.stringify(superheroes, null, 2)
-};
